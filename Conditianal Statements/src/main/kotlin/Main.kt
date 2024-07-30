ab515d8994cda257c7005881e7d9c30eb9f14473
Bundeseule <Bundeseule@PCMH>
2024-07-30 23:34:40 +0200

    small changes

--- Conditianal Statements/src/main/kotlin/Main.kt
+++ Conditianal Statements/src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 fun main() {
     val isActive = true
-    if (isActive == true){
+    if (isActive){   // when false, use !isActive
         println("The user is active")
+    }else{
+        println("The user is not active")
+    }
+
+    var myNumber = 100
+    if (myNumber != 150){   // when false, use !isActive
+        println("Less than or equal 150")
+    }else if (myNumber < 90){
+        println("Greater than 90")
+    }else {
+        println("All the conditions failed")
     }
 }
