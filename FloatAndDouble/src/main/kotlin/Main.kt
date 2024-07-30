7332228d99627e5c65c58d88e1d772143fbfb894
Bundeseule <Bundeseule@PCMH>
2024-07-30 20:13:45 +0200

    Datatypes are done

--- FloatAndDouble/src/main/kotlin/Main.kt
+++ FloatAndDouble/src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
+import kotlin.reflect.typeOf
+
 fun main() {
     val myNumber = 2.5f
-    val mySecondNumber = 2.5
+    val mySecondNumber = 2.0
+
 
     println("My Number is $myNumber and my second Number is $mySecondNumber")
 }
