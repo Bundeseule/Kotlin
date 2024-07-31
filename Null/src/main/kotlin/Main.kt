a363ac9752e817a6610cabe3ae05c0af97c7d929
Bundeseule <Bundeseule@PCMH>
2024-07-31 13:03:57 +0200

    second part of null is done

--- Null/src/main/kotlin/Main.kt
+++ Null/src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
 fun main() {
     var text: String? = "Name"
     text = null
-    if(text != null){
-        println(text.length)
-    }else{
-        println("Text is null")
-    }
+   println(text?.length)
 }
