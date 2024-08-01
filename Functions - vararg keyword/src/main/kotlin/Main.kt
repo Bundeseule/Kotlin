9b783b4eff4d85ff0dcba0fcffbbb2af0f0e0668
Bundeseule <Bundeseule@PCMH>
2024-08-01 02:19:56 +0200

    vararg Done

--- Functions - vararg keyword/src/main/kotlin/Main.kt
+++ Functions - vararg keyword/src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
+    println(sum(5,8,4,10,5,5,8,4,6,5,5,3,7))
+}
 
+fun sum(vararg numbers:Int): Int {
+    var result = 0
+    for (number in numbers) {
+        result += number
+    }
+    return result
 }
