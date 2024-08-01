19bf8d743160399e1719248bb9fa88a1745f02a5
Bundeseule <Bundeseule@PCMH>
2024-08-01 02:12:08 +0200

    Default Values Done

--- Functions - Default Values/src/main/kotlin/Main.kt
+++ Functions - Default Values/src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+    // This are names parameters
+    sendMessage(
+
+        name = "Alexa"
+    )
 }
+
+fun sendMessage(name: String = "User", message: String = sendText()) {
+    println("Sending message to $name: $message")
+}
+
+fun sendText() = "This is a text message"
