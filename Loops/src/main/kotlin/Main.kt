abb5accc6de58dcd2fab2d83230b3f5d2e7f111c
Bundeseule <Bundeseule@PCMH>
2024-08-02 03:29:08 +0200

    for loops done now while loops

--- Loops/src/main/kotlin/Main.kt
+++ Loops/src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
-    println("Hello World!")
+//    for (i in 1..10) {
+//        println(i)
+//    }
+//
+//    for (i in 1 until 10) {
+//        println(i)
+//    }
+//
+//    for(i in 10 downTo 1) {
+//        println(i)
+//    }
+//
+//    for(i in 1 until 10 step 2) {
+//        println(i)
+//    }
+//
+//    for(i in 1..10) {
+//        println(i)
+//    }
+
+    var number = 0
+    while (number < 10) println(number++)
+
+
 }
+
