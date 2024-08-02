efe55d00510dc0beefbf8c91fa73b1c4e730a32e
Bundeseule <Bundeseule@PCMH>
2024-08-02 04:30:04 +0200

    Count and print even numbers from 1 to 20.
    Added a loop to iterate from 1 to 20, printing each even number found. Also implemented a counter to keep track of the number of even numbers identified and displayed the total count at the end. An auxiliary function, isEvenNumber, was created to determine even numbers.

--- loop Challenge/src/main/kotlin/Main.kt
+++ loop Challenge/src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
+    var number = 1
+    val lastNumber = 20
+    var evenNumberCounted  = 0
+
+    while (number <= lastNumber) {
+        number++
+        if (!isEvenNumber(number)) {
+            continue
+        }
+        evenNumberCounted++
+        println(number)
+    }
+    println("Total Numbers of even numbers found: $evenNumberCounted")
+
+
+
+}
+
+fun isEvenNumber(number: Int): Boolean {
+    if ((number % 2) == 0) {
+        return true
+    } else {
+        return false
+    }
 
 }
