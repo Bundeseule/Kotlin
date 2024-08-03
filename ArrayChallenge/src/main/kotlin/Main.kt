ad7bd4eb7cf7365a2d245f2af12c343a36226ee1
Bundeseule <Bundeseule@PCMH>
2024-08-03 14:27:07 +0200

    Refactor min/max functions into single versatile function
    Consolidated the `findMax` and `findMin` functions into a single `findMinAndMax` function with a boolean flag to determine the operation. Simplified the main function to utilize this new method for better code maintainability and reduced redundancy.

--- ArrayChallenge/src/main/kotlin/Main.kt
+++ ArrayChallenge/src/main/kotlin/Main.kt
@@ -1,54 +1,34 @@
-/*
- * This is a simple Kotlin console application designed to search for the maximum and minimum values
- * in an array of integers. It makes use of two custom functions, `findMax()` and `findMin()`.
- */
 fun main() {
-    /*
- * Upon starting the application, the `main()` function initializes an array of fixed integers
- * and the array is passed to the `findMax()` and `findMin()` functions. They respectively calculate
- * and return the maximum and minimum values in the array. The results are then printed to the console.
-*/
-    val max = findMax(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-    val min = findMin(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-    println(
-        "Max Value: $max\n" +
-                "Min Value: $min"
-    )
+
+    val max = findMinAndMax(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,54,16,78,564,5), true)
+    val min = findMinAndMax(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,54,16,78,564,5), false)
+    println("The max value is $max \n"+
+    "The min value is $min")
+
+
 }
 
-// Function to find maximum value
-/*
- * The `findMax()` function takes an array of integers as parameter and returns
- * the maximum value. It iterates over every value in the array, and if it encounters
- * a number that is greater than the current max value, it updates the max value. Finally,
- * it returns the maximum value.
- */
-fun findMax(numbers: Array<Int>): Int {
+
+fun findMinAndMax(numbers: Array<Int>, searchMax: Boolean): Int {
     var max = numbers[0]
-
-    for (number in numbers) {
-        if (number > max) max = number
-
+    var min = max
+    if (searchMax) {
+        for (number in numbers) {
+            if (number > max) {
+                max = number
+            }
+        }
+        return max
+    } else {
+        for (number in numbers) {
+            if (number < min) {
+                min = number
+            }
+        }
+        return min
     }
-
-    return max
 }
 
-// Function to find minimum value
-/*
- * The `findMin()` function operates similarly to `findMax()`, but in contrast,
- * it attempts to find the minimum value in an array of integers. It iterates over each
- * number, updating the minimum value if it encounters a number less than the current min value.
- * Finally, it returns the minimum value.
- */
-fun findMin(numbers: Array<Int>): Int {
-    var min = numbers[0]
 
-    for (number in numbers) {
-        if (number < min) min = number
 
-    }
-
-    return min
-}
 
