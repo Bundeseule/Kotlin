e076ca4163bdb0014af48539f2be057802cef382
Bundeseule <Bundeseule@PCMH>
2024-08-03 13:30:08 +0200

    Add dictionary configuration and update main function
    Created a new XML file for dictionary settings in the project. Modified the main function to initialize an array of names, print the first element, update it, and print the updated value.

--- Arrays/src/main/kotlin/Main.kt
+++ Arrays/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
-fun main() {
-    println("Hello World!")
+fun main(args: Array<String>) {
+    val names = arrayOf("John", "Stephen", "Megan")
+    println(" Frist Element: ${names[0]}")
+
+    names[0] = "Alex"
+    println(" Frist Element: ${names[0]}")
 }
