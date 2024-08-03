0e3609455c09dc0f60978e9a1518297fd4235fa1
Bundeseule <Bundeseule@PCMH>
2024-08-03 14:13:00 +0200

    Add functions to find max and min values in an array
    Introduced `findMax` and `findMin` functions to compute the maximum and minimum values in an array, respectively. Updated the main function to print these values using the new functions. This enhances the array handling capabilities of the program.

--- ArrayChallenge/src/main/kotlin/Main.kt
+++ ArrayChallenge/src/main/kotlin/Main.kt
@@ -1,3 +1,31 @@
 fun main() {
-
+    val max = findMax(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+    val min = findMin(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+    println(
+        "Max Value: $max\n" +
+                "Min Value: $min"
+    )
 }
+
+fun findMax(numbers: Array<Int>): Int {
+    var max = numbers[0]
+
+    for (number in numbers) {
+        if (number > max) max = number
+
+    }
+
+    return max
+}
+
+fun findMin(numbers: Array<Int>): Int {
+    var min = numbers[0]
+
+    for (number in numbers) {
+        if (number < min) min = number
+
+    }
+
+    return min
+}
+
