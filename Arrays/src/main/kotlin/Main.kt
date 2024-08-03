36c634c33bf36deab7f264dd38d0b0154955b489
Bundeseule <Bundeseule@PCMH>
2024-08-03 13:33:56 +0200

    Add array iteration and size printing in main function
    Refactored the main function to include arrays for numbers and names. Added logic to print the size of the 'names' array and iteratively print elements of both 'names' and 'numbers' arrays.

--- Arrays/src/main/kotlin/Main.kt
+++ Arrays/src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
 fun main(args: Array<String>) {
     val names = arrayOf("John", "Stephen", "Megan")
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Name 1", "2")
     println(" Frist Element: ${names[0]}")
 
     names[0] = "Alex"
     println(" Frist Element: ${names[0]}")
+
+    println("The size of the element is: ${names.size}")
+
+    for(name in names) {
+        println(name)
+    }
+
+    for (i in numbers){
+        println(i)
+    }
+
 }
