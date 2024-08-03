a3d7eafd1c31e34d8e390f299dac94db2efce2fe
Bundeseule <Bundeseule@PCMH>
2024-08-03 14:41:42 +0200

    Add new OOP module with Kotlin main function
    Created 'OOP' directory and added a basic empty Kotlin main function. Configured OOP module with source and resource folders in OOP.iml. Updated modules.xml to include the new OOP module.

--- OOP/src/main/kotlin/Main.kt
+++ OOP/src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
 fun main() {
+    val car1=Car()
+    car1.name = "Tesla"
+    car1.model ="S Plaid"
+    car1.color = "Red"
+    car1.doors = 4
 
+    println("Name is ${car1.name}")
+    println("Model is ${car1.model}")
+    println("Color is ${car1.color}")
+    println("Doors is ${car1.doors}")
+
+    car1.move()
+    car1.stop()
+}
+
+class Car {
+    var name = ""
+    var model = ""
+    var color = ""
+    var doors = 0
+
+    fun move(){
+        println("The car $name is moving...")
+    }
+
+    fun stop() {
+        println("The car $name has stopped")
+    }
 }
