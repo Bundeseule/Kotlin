7ce4aad0a6d27e95b7a88e7e3970c1c424115c94
Bundeseule <Bundeseule@PCMH>
2024-08-05 00:35:01 +0200

    Add new module for OOP initialization blocks
    Created a new module "OOP - Initialize Blocks" with corresponding Kotlin source files and updated the project configuration. This module introduces a Car class featuring a trimmed name property and basic operations like move and stop. Updated the existing OOP module to align with these changes.

--- OOP - Initialize Blocks/src/main/kotlin/Main.kt
+++ OOP - Initialize Blocks/src/main/kotlin/Main.kt
@@ -24,15 +24,5 @@ fun main() {
 
 }
 
-class Car(name: String, var model: String, var color: String, var doors: Int) {
 
-    var name = name.trim()
 
-    fun move() {
-        println("The car $name is moving...")
-    }
-
-    fun stop() {
-        println("The car $name has stopped")
-    }
-}
