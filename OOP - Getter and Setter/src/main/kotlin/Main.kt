8b4be1c946ccf02cfbc92bc39e68e293fb8c7dc2
Bundeseule <Bundeseule@PCMH>
2024-08-05 16:27:12 +0200

    Add new User class and setup IntelliJ module
    Introduce a `User` class with basic properties and a main function to create instances and print their fields. Also, configure the new module in IntelliJ's project XML files.

---
+++ OOP - Getter and Setter/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val user = User("Alex")
+    val friend = User(
+        firstName = "John",
+        lastName = "Smith"
+    )
+    val user2= User(age = 19, firstName = "Iona", lastName = "Berrington")
+
+    println("Name: ${user.firstName}")
+    println("Age: ${user.age}")
+    println("Age: ${user.age}")
+
+    println("\n")
+
+    println("Name: ${friend.firstName}")
+    println("LastName: ${friend.lastName}")
+    println("Age: ${friend.age}")
+}
+
+class User(var firstName: String, var lastName: String = "LastName", var age: Int = 0) {
+
+
+}
