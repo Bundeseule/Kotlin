cf61f9a1da7e06b7e4aedbf6e15d17745178ec72
Bundeseule <Bundeseule@PCMH>
2024-08-17 02:29:41 +0200

    Add Enum module and update Singleton lazy initialization
    Introduced a new Enum module with an enum class for directions. Updated the Singleton module to use lazy initialization for the second user instance to improve performance.

--- Enum/src/main/kotlin/Main.kt
+++ Enum/src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
 fun main() {
-    println(Direction.WEST)
-    println(Direction.EAST)
-    println(Direction.NORTH)
-    println(Direction.SOUTH)
+    for(direction in Direction.values()){
+        println(direction.direction)
+    }
 }
 
-enum class Direction{
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST
+enum class Direction(var direction: String, var distance:Int) {
+    NORTH("north", 90){},
+    SOUTH("south", 180),
+    EAST("east", 270),
+    WEST("west", 90),
 }
