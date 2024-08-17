fffe73f22ac80ebb038b6a167fd558d9b1972e77
Bundeseule <Bundeseule@PCMH>
2024-08-17 02:17:46 +0200

    Refactor singleton implementation to Kotlin object
    Replaced singleton class with Kotlin object to make use of Kotlin's built-in singleton functionality. This change ensures that the Database instance is thread-safe and created only once without the need for explicit synchronization. Comments were added to previously used singleton methods and variables for future reference.

--- Singleton/src/main/kotlin/Main.kt
+++ Singleton/src/main/kotlin/Main.kt
@@ -1,19 +1,29 @@
 fun main() {
 
-    val instance = Database.getInstance()
-    println(instance)
+//    val instance = Database.getInstance()
+//    val instance2 = Database.getInstance()
+//    println(instance2)
+//    println(instance)
+    println(Database)
 }
 
-class Database private constructor() {
+//class Database private constructor() {
+//
+//    companion object {
+//        private var instance: Database? = null
+//
+//        fun getInstance(): Database? {
+//            if (instance == null) {
+//                instance = Database()
+//            }
+//            return instance
+//        }
+//    }
+//}
 
-    companion object {
-        private var instance: Database? = null
-
-        fun getInstance(): Database? {
-            if (instance == null) {
-                instance = Database()
-            }
-            return instance
-        }
+object Database {
+    init {
+        println("Created Database")
     }
 }
+
