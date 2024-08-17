e36e076c62bb1e88be4c0f0f821eff1a390d6134
Bundeseule <Bundeseule@PCMH>
2024-08-17 02:08:48 +0200

    Add Singleton module and update Calculator behavior
    Added the Singleton module configuration to the project. Modified `Main.kt` and `Calculator` class to include a max value and print the sum.

--- Init/src/main/kotlin/Main.kt
+++ Init/src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
 fun main() {
 
-    Calculator.sum(5,10)
-
+    val result = Calculator.sum(5,10)
+    Calculator.max
+    val max = Int.MAX_VALUE
+    println(result)
 
 }
 
 class Calculator(){
     companion object{
+        var max = 100
         fun sum(a: Int, b: Int): Int {
             return a + b
         }
