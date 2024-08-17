2e3765e6397e3494be103e7ee318365d866d007d
Bundeseule <Bundeseule@PCMH>
2024-08-17 02:32:50 +0200

    Add printData function to Direction enum
    Enhanced the Direction enum with a printData function that prints the direction and distance. This additional method improves the ability to log or display enum values effectively within the application.

--- Enum/src/main/kotlin/Main.kt
+++ Enum/src/main/kotlin/Main.kt
@@ -2,11 +2,17 @@ fun main() {
     println(Direction.NORTH.direction)
     println(Direction.NORTH.distance)
     println(Direction.NORTH.name)
+
+    Direction.WEST.printData()
 }
 
 enum class Direction(var direction: String, var distance:Int) {
     NORTH("north", 90){},
     SOUTH("south", 180),
     EAST("east", 270),
-    WEST("west", 90),
+    WEST("west", 90);
+
+    fun printData(){
+        println("Direction = $direction, distance = $distance")
+    }
 }
