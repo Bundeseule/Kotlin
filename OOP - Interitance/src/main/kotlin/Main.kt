83a6df0e9a9d2fa6c909276a009ca55122bcf1cf
Bundeseule <Bundeseule@PCMH>
2024-08-21 01:48:56 +0200

    Add Car and Plane classes with movement methods
    Implemented Car and Plane classes, each with move and stop methods. This allows instantiation of these objects and control over their basic actions. This will serve as a foundation for further development in the project.

--- OOP - Interitance/src/main/kotlin/Main.kt
+++ OOP - Interitance/src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
-    println("Hello World!")
+
+}
+
+class Car(val name:String,val color:String,val engines:Int, val doors: Int){
+    fun move(){
+        println("$name is moving")
+    }
+
+    fun stop(){
+        println("$name is stopped")
+    }
+}
+
+class Plane(val name:String,val color:String,val engines:Int,val doors: Int){
+    fun move(){
+        println("$name is moving")
+    }
+
+    fun stop(){
+        println("$name is stopped")
+    }
 }
