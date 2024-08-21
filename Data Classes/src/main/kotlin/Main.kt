1c6fc400ca105881383f635caffb1a443a379e1d
Bundeseule <Bundeseule@PCMH>
2024-08-21 16:48:21 +0200

    Format code by adding spaces around colons and braces
    Standardized the code style by ensuring spaces around colons for property definitions and braces for class and interface implementations. This enhances readability and maintains consistency throughout the codebase.

--- Data Classes/src/main/kotlin/Main.kt
+++ Data Classes/src/main/kotlin/Main.kt
@@ -3,31 +3,31 @@ fun main() {
 
 }
 
-interface Engine{
+interface Engine {
     fun startEngine()
 
 }
 
-class Car(val name:String, val color:String):Engine{
+class Car(val name: String, val color: String) : Engine {
     override fun startEngine() {
         println("Car is starting...")
     }
 
 }
 
-class Truck(val name:String, val color: String):Engine{
+class Truck(val name: String, val color: String) : Engine {
     override fun startEngine() {
         println("Truck is starting...")
     }
 }
 
-class Plane(val name:String, val color:String):Engine{
+class Plane(val name: String, val color: String) : Engine {
     override fun startEngine() {
         println("Plane is starting...")
     }
 }
 
-class Tesla(val name:String, val color:String):Engine{
+class Tesla(val name: String, val color: String) : Engine {
     override fun startEngine() {
         println("Tesla is starting...")
     }
