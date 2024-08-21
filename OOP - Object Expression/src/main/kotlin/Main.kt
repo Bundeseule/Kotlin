e2c1837831a8d4eec2db247b0edd9c8761818c40
Bundeseule <Bundeseule@PCMH>
2024-08-21 17:01:54 +0200

    Add login and signup buttons with OnClickListeners
    This commit introduces two buttons, Login and Sign Up, each with their respective OnClickListeners. The buttons currently have placeholders for login and signup functionality, which need to be implemented.

--- OOP - Object Expression/src/main/kotlin/Main.kt
+++ OOP - Object Expression/src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
 fun main() {
 
+
+    val loginButton = Button("Login", 1232, object : OnClickListener {
+        override fun onClick() {
+            //TODO: login the user
+        }
+    })
+    val signUpButton = Button("Sign Up", 1231, object : OnClickListener {
+        override fun onClick() {
+            //TODO: sign up the user
+        }
+    })
+}
+
+class Button(val text:String, val id:Int, onClickListener:OnClickListener)
+
+class ClickListener():OnClickListener{
+    override fun onClick() {
+
+    }
+}
+
+interface OnClickListener{
+    fun onClick()
 }
