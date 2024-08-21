35a081bf916cda8289a4911d77e940c33ecfa170
Bundeseule <Bundeseule@PCMH>
2024-08-21 02:54:32 +0200

    Add Data Classes module and rename Sealed Class module
    Introduced a new module for Data Classes with necessary configurations and added an initial Kotlin file. Also, renamed the Sealed Class module to OOP - Sealed Class to reflect a more descriptive module naming convention.

--- Data Classes/src/main/kotlin/Main.kt
+++ Data Classes/src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
 fun main() {
+    val user1 = User("Alex","Dobin",23)
+    val user2 = User("Alex","Dobin",23)
 
+    println(user1.equals(user2))
+
+
+}
+
+class User(var firstName: String, var lastName: String, var age: Int) {
+    override fun equals(other: Any?): Boolean {
+        if(this===other){
+            return true
+        }
+
+        if(other is User){
+            return this.firstName == other.firstName && this.lastName == other.lastName && this.age == other.age
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return 0
+    }
 }
