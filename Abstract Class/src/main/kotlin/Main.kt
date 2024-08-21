471931616f84b94c810299d37d8234acb59858bd
Bundeseule <Bundeseule@PCMH>
2024-08-21 02:39:46 +0200

    Add abstract Vehicle class and Car implementation
    Introduce an abstract Vehicle class with move and stop methods to define common vehicle behaviors. Implement the Car class that inherits from Vehicle, adding properties like name, color, engine, and doors.

--- Abstract Class/src/main/kotlin/Main.kt
+++ Abstract Class/src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
 fun main() {
-    println("Hello World!")
+
+}
+
+abstract class Vehicle(){
+
+    abstract fun move()
+
+
+
+    abstract fun stop()
+
+
+}
+
+class Car(var name:String, var color:String,val engine :String, val doors:Int): Vehicle(){
+    override fun move() {
+
+    }
+
+    override fun stop() {
+
+    }
+
 }
