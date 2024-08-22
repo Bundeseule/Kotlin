f2aeb4bc259c15dfa2d6f3450a40e15fceb8f5ad
Bundeseule <Bundeseule@PCMH>
2024-08-22 21:09:48 +0200

    Refactor variable names and add list operations
    Renamed the 'numbers' variable to 'numbersStrings' for clarity. Added new list operations: chunked and windowed, to demonstrate more complex collection manipulations. These additions enhance functionality and readability.

--- Retrieve Collection parts/src/main/kotlin/Main.kt
+++ Retrieve Collection parts/src/main/kotlin/Main.kt
@@ -1,18 +1,26 @@
 fun main(args: Array<String>) {
-    val numbers = listOf("one", "two", "three", "four", "five", "six")
-    println(numbers.slice(1..3))
-    println(numbers.slice(0..4 step 2))
-    println(numbers.slice(setOf(3,5,0)))
+    val numbersStrings = listOf("one", "two", "three", "four", "five", "six")
+    println(numbersStrings.slice(1..3))
+    println(numbersStrings.slice(0..4 step 2))
+    println(numbersStrings.slice(setOf(3,5,0)))
 
     println("\n")
-    println(numbers.take(3))
-    println(numbers.takeLast(3))
-    println(numbers.drop(1))
-    println(numbers.dropLast(5))
+    println(numbersStrings.take(3))
+    println(numbersStrings.takeLast(3))
+    println(numbersStrings.drop(1))
+    println(numbersStrings.dropLast(5))
 
     println("\n")
-    println(numbers.takeWhile { !it.startsWith("f") })
-    println(numbers.takeLastWhile { it != "three" })
-    println(numbers.dropWhile{it.length == 3})
-    println(numbers.dropLastWhile{it.contains("i")})
+    println(numbersStrings.takeWhile { !it.startsWith("f") })
+    println(numbersStrings.takeLastWhile { it != "three" })
+    println(numbersStrings.dropWhile{it.length == 3})
+    println(numbersStrings.dropLastWhile{it.contains("i")})
+
+    val numbers = (0..13).toList()
+    println(numbers.chunked(3))
+    println(numbers.chunked(3){it.sum()})
+
+    println("\n")
+    val numbersString2=numbersStrings
+    println(numbersString2.windowed(3))
 }
