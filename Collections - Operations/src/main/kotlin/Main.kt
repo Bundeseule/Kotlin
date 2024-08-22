45e3c14da43ef16b838402607552ae5d36c1538e
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:41:44 +0200

    Update multiplication logic in main function
    Changed the multiplication factor for the number 3 to 100 instead of 10. This adjustment ensures that the number 3 will be distinctly amplified compared to other elements in the set.

--- Collections - Operations/src/main/kotlin/Main.kt
+++ Collections - Operations/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
 fun main(args: Array<String>) {
     val numbers = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-    println(numbers.map{it*10})
+    println(numbers.map{if(it==3) it * 100 else it * 10})
 }
