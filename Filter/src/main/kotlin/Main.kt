2c3613dea72bad3bfa161c71f0c434dfb4fc04e8
Bundeseule <Bundeseule@PCMH>
2024-08-22 03:27:48 +0200

    Add filtering logic for a map based on key and value
    Added code to filter a map where the keys end with '1' and the values are greater than 100. This change complements the existing list filtering functionality and enhances the overall data manipulation capabilities of the application.

--- Filter/src/main/kotlin/Main.kt
+++ Filter/src/main/kotlin/Main.kt
@@ -6,4 +6,9 @@ fun main() {
     val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key101" to 101)
     val filteredMap = numbersMap.filter{it.key.endsWith("1") && it.value > 100}
     println(filteredMap)
+
+    val filteredIndex = numbers.filterIndexed{index, value -> (index != 0)&& (value.length<5) }
+    val filteredNot = numbers.filterNot {it.length <= 3}
+    println(filteredIndex)
+    println(filteredNot)
 }
