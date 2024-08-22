43a1bb41674d2755849581f104ee26525df8481e
Bundeseule <Bundeseule@PCMH>
2024-08-22 20:55:09 +0200

    Add additional slice and take/drop operations to main function
    Expand the Main.kt function to include more slice variations and add take/drop operations for better demonstration of list functionalities. This enhances the readability and utility of the example code.

--- Retrieve Collection parts/src/main/kotlin/Main.kt
+++ Retrieve Collection parts/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
     val numbers = listOf("one", "two", "three", "four", "five", "six")
     println(numbers.slice(1..3))
-    println(numbers.slice(1..4 step 2))
+    println(numbers.slice(0..4 step 2))
     println(numbers.slice(setOf(3,5,0)))
+
+    println("\n")
+    println(numbers.take(3))
+    println(numbers.takeLast(3))
+    println(numbers.drop(1))
+    println(numbers.dropLast(5))
 
 }
