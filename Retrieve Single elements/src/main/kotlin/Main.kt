bdf826f7243d3b47fa8c43c5fa4af9f9640ed5f0
Bundeseule <Bundeseule@PCMH>
2024-08-22 21:42:50 +0200

    Add new element retrieval methods
    Enhanced Main.kt by adding methods to retrieve the first element with length greater than 3, the last element starting with 'f', and a random element. These additions improve the functionality and flexibility of element retrieval from the list.

--- Retrieve Single elements/src/main/kotlin/Main.kt
+++ Retrieve Single elements/src/main/kotlin/Main.kt
@@ -3,4 +3,10 @@ fun main(args: Array<String>) {
     println(numbers.elementAt(3))
     println(numbers.first())
     println(numbers.last())
+
+    println("\n")
+    println(numbers.first{it.length>3})
+    println(numbers.last{it.startsWith("f")})
+    println(numbers.random())
+
 }
