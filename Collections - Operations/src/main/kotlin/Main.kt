7640d1106b9298f85cf1364d60234a2db9fd1063
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:44:28 +0200

    Add examples for transforming map keys and values
    Introduced two new examples in `main` function to demonstrate how to transform keys and values in a map. The first example converts all keys to uppercase, while the second example increments each value by the length of its key.

--- Collections - Operations/src/main/kotlin/Main.kt
+++ Collections - Operations/src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 fun main(args: Array<String>) {
     val numbers = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
     println(numbers.map{if(it==3) it * 100 else it * 10})
+
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 4, "key5" to 5)
+    println(numbersMap.mapKeys{it.key.uppercase()})
+    println(numbersMap.mapValues{it.value + it.key.length})
 }
