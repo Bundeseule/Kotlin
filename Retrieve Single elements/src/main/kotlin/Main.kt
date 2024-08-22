577693e0f9ec019c38db1ff73b72d79d43f1e05b
Bundeseule <Bundeseule@PCMH>
2024-08-22 21:12:29 +0200

    Add first and last element prints in Main.kt
    This commit updates the main function to print the first and last elements of the list in addition to the existing element at index 3. This enhancement provides more insights into the list contents during execution.

--- Retrieve Single elements/src/main/kotlin/Main.kt
+++ Retrieve Single elements/src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 fun main(args: Array<String>) {
     val numbers = listOf("one", "two", "three", "four", "five", "six")
     println(numbers.elementAt(3))
+    println(numbers.first())
+    println(numbers.last())
 }
