cb38a5f910c437982749349ee8a3bca1113f2dcc
Bundeseule <Bundeseule@PCMH>
2024-08-22 03:04:26 +0200

    Refactor collection operations in Main.kt
    Replaced nested arrays with sets within a list for better readability and iterated through them using nested loops. Also improved code formatting for consistency and readability by adding spaces and adjusting indentation.

--- Collections - Operations/src/main/kotlin/Main.kt
+++ Collections - Operations/src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 fun main(args: Array<String>) {
     val numbers = listOf("one", "two", "three", "four", "five", "six")
     println(numbers.associateWith { it.length })
-    println(numbers.associateWith{it.first().uppercase()})
-    println(numbers.associateBy(keySelector = {it.first().uppercase()}, valueTransform = {it.length}))
+    println(numbers.associateWith { it.first().uppercase() })
+    println(numbers.associateBy(keySelector = { it.first().uppercase() }, valueTransform = { it.length }))
 
     //Flatten
-    val numberSets = arrayOf(arrayOf(1, 2, 3), arrayOf(4, 5, 6), arrayOf(7, 8, 9))
-    println(numberSets[2][2])
+    val numberSets = listOf(setOf(1, 2, 3), setOf(4, 5, 6), setOf(7, 8, 9))
+    for (numbers in numberSets) {
+        for (number in numbers)
+            println(number)
+    }
+    println("\n")
 }
