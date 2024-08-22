72f25af9850d544d750b317d6962cc568cfc9fc0
Bundeseule <Bundeseule@PCMH>
2024-08-22 03:15:40 +0200

    Add string and number list representations in main
    Introduced a list of strings and demonstrated various string operations like joining and formatting. Also created a list of numbers from 1 to 100 and printed it for verification.

--- String Representation/src/main/kotlin/Main.kt
+++ String Representation/src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
+    val numbersStrings = listOf("one", "two", "three", "four", "five", "six", "seven", "eight")
+    println(numbersStrings)
+    println(numbersStrings.joinToString())
 
+    val listString = StringBuffer("The list of numbers: ")
+    println(numbersStrings.joinTo(listString))
+    println(numbersStrings.joinToString(separator = " | ", prefix = ":", postfix = ": end"))
+    val numbers = (1 .. 100).toList()
+    println(numbers)
 }
