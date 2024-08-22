0174ffc5ff04659572b4fe8472e200ee8bedca91
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:51:44 +0200

    Add example for unzipping pairs in Main.kt
    This commit introduces a new list of number pairs and prints the result of unzipping them. It enhances the existing examples by demonstrating how to use the unzip function with paired elements.

--- Collections - Operations/src/main/kotlin/Main.kt
+++ Collections - Operations/src/main/kotlin/Main.kt
@@ -12,4 +12,8 @@ fun main(args: Array<String>) {
     val animals=listOf("fox","bear","wolf")
     println(colors.zip(animals))
     println(colors.zip(animals){color, animal -> "The ${animal.replaceFirstChar { it.uppercase() }} is $color"})
+
+    val numberPairs = listOf("one" to 1, "two" to 2, "three" to 3, "four" to 4)
+    println(numberPairs)
+    println(numberPairs.unzip())
 }
