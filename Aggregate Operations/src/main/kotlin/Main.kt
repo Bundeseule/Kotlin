5df35354ef7fd209e56e75c3700b62e4ee177b72
Bundeseule <Bundeseule@PCMH>
2024-08-22 21:47:54 +0200

    Add 'Aggregate Operations' module
    Included new entry in the .idea/modules.xml for 'Aggregate Operations'. Created the module's .iml file and a placeholder Main.kt file in the src directory.

--- Aggregate Operations/src/main/kotlin/Main.kt
+++ Aggregate Operations/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
+    val numbers = listOf(4, 2, 6, 88, 3, 1, 0, 100)
+    println("The sum is ${numbers.sum()}")
+    println("The count is ${numbers.count()}")
+    println("The average is ${numbers.average()}")
+    println("The max Value is ${numbers.maxOrNull()}")
+    println("The min Value is ${numbers.minOrNull()}")
 
 }
