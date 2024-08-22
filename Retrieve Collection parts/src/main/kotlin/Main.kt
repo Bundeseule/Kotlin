c338b3e476a2feaaafefad482906fc01119d7179
Bundeseule <Bundeseule@PCMH>
2024-08-22 20:52:35 +0200

    Add slicing examples to main function
    Introduced a list of numbers and added println statements to demonstrate slicing operations. These include slicing a range, slicing with a step, and slicing using a set of indices.

--- Retrieve Collection parts/src/main/kotlin/Main.kt
+++ Retrieve Collection parts/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>) {
+    val numbers = listOf("one", "two", "three", "four", "five", "six")
+    println(numbers.slice(1..3))
+    println(numbers.slice(1..4 step 2))
+    println(numbers.slice(setOf(3,5,0)))
 
 }
