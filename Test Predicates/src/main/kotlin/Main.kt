0ef3fa41517180e45233061b7f2a81a71c4a0870
Bundeseule <Bundeseule@PCMH>
2024-08-22 13:07:16 +0200

    Add Test Predicates module
    Introduced a new module "Test Predicates" with basic structure. Included configuration in `.idea/modules.xml` and set up folder structure in the corresponding `.iml` file. This prepares the project for upcoming test predicate functionalities.

--- Test Predicates/src/main/kotlin/Main.kt
+++ Test Predicates/src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
 fun main() {
+    val numbers = listOf("one", "two", "three", "four", "five", "six")
+    val longerThan3 = numbers.filter{it.length > 3}
+    println(longerThan3)
+
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key101" to 101)
+    val filteredMap = numbersMap.filter{it.key.endsWith("1") && it.value > 100}
+    println(filteredMap)
+
+    val filteredIndex = numbers.filterIndexed{index, value -> (index != 0)&& (value.length<5) }
+    val filteredNot = numbers.filterNot {it.length <= 3}
+    println(filteredIndex)
+    println(filteredNot)
+
+    val mixedList = listOf(1,2,3,'A','B','C','D','E','F',"Alex","World Hello", false)
+    mixedList.filterIsInstance<Boolean>().forEach{
+        println(it)
+    }
+    //Partition
+    println("\n")
+    val (match, rest) = numbers.partition{it.length > 3}
+    println(match)
+    println(rest)
+    println(numbers.any{it.endsWith("e")})
+    println(numbers.none{it.endsWith("w")})
+    println(numbers.all{it.length > 1})
 
 }
