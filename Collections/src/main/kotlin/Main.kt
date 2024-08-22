5149ee4a84237ea61c5642d19ad194c61e5d864f
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:33:55 +0200

    Refactor user initialization and printing
    Replaced individual user instances with a map and simplified the printing of user names by directly accessing the map based on key. This change streamlines the code and improves readability.

--- Collections/src/main/kotlin/Main.kt
+++ Collections/src/main/kotlin/Main.kt
@@ -1,17 +1,7 @@
 fun main() {
-    val user1 = User("Name 1")
-    val user2 = User("Name 2")
-    val user3 = User("Name 3")
-    val user4 = User("Name 4")
-    val user5 = User("Name 5")
-    val user6 = User("Alex")
-    val user7 = User("Alex")
+    val users = mapOf<Int, String>(1 to "Maria", 2 to "Alex", 3 to "John")
 
-    val names = mutableSetOf<User>(user1, user2, user3, user4, user5, user6, user7)
-
-    names.forEach { (println(it.name)) }
+    println(users[1])
 }
 
-data class User(val name: String) {
 
-}
