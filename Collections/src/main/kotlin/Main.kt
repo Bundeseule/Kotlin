9348ddf10cf03d2c246b58690517531d433fe012
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:31:54 +0200

    Refactor name set handling to use User data class
    Replaced string-based set with a set of User objects to enhance data structure. Added a User data class and updated the main function to handle instances of this class, ensuring that unique names are printed even when duplicates are added to the set.

--- Collections/src/main/kotlin/Main.kt
+++ Collections/src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
 fun main() {
-    val names = setOf("Name 1", "Name 2", "Name 3", "Name 1")
+    val user1 = User("Name 1")
+    val user2 = User("Name 2")
+    val user3 = User("Name 3")
+    val user4 = User("Name 4")
+    val user5 = User("Name 5")
+    val user6 = User("Alex")
+    val user7 = User("Alex")
 
+    val names = mutableSetOf<User>(user1, user2, user3, user4, user5, user6, user7)
 
+    names.forEach { (println(it.name)) }
+}
 
+data class User(val name: String) {
 
-    names.forEach { (println(it)) }
 }
