3992de5382fc34d7064b53ea1533507f8583efc0
Bundeseule <Bundeseule@PCMH>
2024-08-22 20:50:22 +0200

    Add Retrieve Collection parts module and enhance Grouping main
    Added a new Kotlin module called 'Retrieve Collection parts' and its configuration files. Enhanced the existing Grouping.main.kt to include grouping operations on a list of strings.

--- Grouping/src/main/kotlin/Main.kt
+++ Grouping/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>) {
-
+    val numbers = listOf("one", "two", "three", "four", "five", "six")
+    println(numbers.groupBy { it.first().uppercase() })
+    println(numbers.groupBy(keySelector = { it.first().uppercase() }, valueTransform = { it.uppercase() }))
 }
