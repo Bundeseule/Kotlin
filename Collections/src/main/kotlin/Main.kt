c16c3c289a42559620294718039b79d3fe811d49
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:37:16 +0200

    Convert users map to mutable and print in a loop
    Changed the users map from immutable to mutable to allow future modifications. Updated the print statement to iterate over the map, displaying each key-value pair.

--- Collections/src/main/kotlin/Main.kt
+++ Collections/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
     val users = mutableMapOf<Int, String>(1 to "Maria", 2 to "Alex", 3 to "John")
-
+    users[5] = "Vlad"
+    users.remove(2)
     users.forEach { t,u ->
         println("$t: $u")}
 
