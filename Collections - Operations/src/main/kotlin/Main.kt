8f87d6b139082df650bf119844c7ebbce4a8ca85
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:45:58 +0200

    Add mapIndexed transformation for numbers set
    Included a new mapIndexed operation which multiplies each element by its index, setting the first element to null. This enhances the functionality by providing an additional transformation example for the set.

--- Collections - Operations/src/main/kotlin/Main.kt
+++ Collections - Operations/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val numbers = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
     println(numbers.map{if(it==3) it * 100 else it * 10})
-
+    println(numbers.mapIndexed { index, value -> if(index==0)null else index * value })
     val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 4, "key5" to 5)
     println(numbersMap.mapKeys{it.key.uppercase()})
     println(numbersMap.mapValues{it.value + it.key.length})
