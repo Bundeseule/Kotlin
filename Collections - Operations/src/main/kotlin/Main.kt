a900b7bb2c5cde9c6c014803cbcf532b04bddeb5
Bundeseule <Bundeseule@PCMH>
2024-08-22 03:01:41 +0200

    Add array initialization example in Main.kt
    Extended the Main.kt file to include an initialization and access example of a 2D array named `numberSets`. This demonstrates how to create and retrieve elements from a nested array structure in Kotlin.

--- Collections - Operations/src/main/kotlin/Main.kt
+++ Collections - Operations/src/main/kotlin/Main.kt
@@ -3,4 +3,8 @@ fun main(args: Array<String>) {
     println(numbers.associateWith { it.length })
     println(numbers.associateWith{it.first().uppercase()})
     println(numbers.associateBy(keySelector = {it.first().uppercase()}, valueTransform = {it.length}))
+
+    //Flatten
+    val numberSets = arrayOf(arrayOf(1, 2, 3), arrayOf(4, 5, 6), arrayOf(7, 8, 9))
+    println(numberSets[2][2])
 }
