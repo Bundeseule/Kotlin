d05819db08f0485e98ef8d7c2c25c81fc97067d5
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:14:58 +0200

    Add delegation to classes and implement FormatDelegate
    Introduced delegation in `App` class through `FirstDelegate` and `SecondDelegate`. Defined `FormatDelegate` for string formatting which converts strings to lowercase. It enhances code modularity and promotes reusability.

--- OOP - Delegation/src/main/kotlin/Main.kt
+++ OOP - Delegation/src/main/kotlin/Main.kt
@@ -1,4 +1,56 @@
+import kotlin.reflect.KProperty
+import kotlin.properties.ReadWriteProperty
+
 fun main() {
 
 }
 
+class App: A by FirstDelegate(),B by SecondDelegate(){
+    override fun print() {
+        TODO("Not yet implemented")
+    }
+
+    override fun print2() {
+        TODO("Not yet implemented")
+    }
+}
+
+interface A {
+    fun print()
+}
+
+interface B {
+    fun print2()
+}
+
+open class FirstDelegate: A{
+    override fun print() {
+
+    }
+}
+
+open class SecondDelegate: B{
+    override fun print2() {
+
+    }
+}
+
+class FormatDelegate: ReadWriteProperty<Any?, String>{
+    private var formattedString: String = ""
+
+    override fun getValue(
+        this.Ref: Any?,
+        property: KProperty<*>
+    ): String {
+        return formattedString
+    }
+
+    override fun setValue(
+        thisRef: Any?,
+        property: KProperty<*>,
+        value: String
+    )
+    {
+        formattedString = value.lowercase()
+    }
+}
