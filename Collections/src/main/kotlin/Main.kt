ea62e59c95d7a06868b4ee930645c7b666aadec4
Bundeseule <Bundeseule@PCMH>
2024-08-22 02:27:10 +0200

    Modify main function to update and print name list
    Added a mutable list of names and performed operations to add and remove elements. Iterated through the list to print each name to the console.

--- Collections/src/main/kotlin/Main.kt
+++ Collections/src/main/kotlin/Main.kt
@@ -1,8 +1,7 @@
 fun main() {
-    val names = mutableListOf("Name 1", "Name 2", "Name 3")
+    val names = setOf("Name 1", "Name 2", "Name 3", "Name 1")
 
-    names.add("Name 4")
-    names.remove("Name 2")
+
 
 
     names.forEach { (println(it)) }
